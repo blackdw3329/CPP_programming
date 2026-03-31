5dd9f1ec31414cfaba12f9005188051fd6ae8077
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-03-31 14:44:26 +0900

    Add files via upload

---
+++ 3-5/cpp3_5_4.cpp
@@ -0,0 +1,41 @@
+// **********************************************
+// 제 목 : 3_5 실습과제4
+// 날 짜 : 2026년 3월 30일
+// 작성자 : 2301358 김동욱
+// **********************************************
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+class Picture
+{
+public:
+	Picture() {
+		width = 5;
+		height = 7;
+		strcpy(location, "모름");
+	}
+	Picture(int w, int h, const char* loca) {
+		width = w;
+		height = h;
+		strcpy(location, loca);
+	}
+	~Picture() {}
+
+	int getWidth() { return width; }
+	int getHeight() { return height; }
+	const char* getPlace() { return location; }
+
+private:
+	char location[100];
+	int width, height;
+};
+
+int main()
+{
+	Picture pic;
+	Picture mt(10, 14, "한라산");
+	cout << pic.getWidth() << "x" << pic.getHeight() << " " << pic.getPlace() << endl;
+	cout << mt.getWidth() << "x" << mt.getHeight() << " " << mt.getPlace() << endl;
+}
---
+++ 3-5/cpp3_5_5.cpp
@@ -0,0 +1,56 @@
+// **********************************************
+// 제 목 : 3_5 실습과제5
+// 날 짜 : 2026년 3월 30일
+// 작성자 : 2301358 김동욱
+// **********************************************
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+class Storage
+{
+public:
+	Storage() {
+		for (int i = 0; i < 10; i++) {
+			average_total[i] = 0.0;
+		}
+		count = 0; 
+	}
+	~Storage() {}
+	void put(double temp);
+	double getAvg();
+	void dump();
+
+private:
+	double average_total[10];
+	int count;
+};
+
+void Storage::put(double temp)
+{
+	if (count < 10) { average_total[count] += temp; count++; }
+	else cout << "최대 10회를 초과 하였습니다." << endl;
+}
+double Storage::getAvg()
+{
+	double total = 0;
+	for (int j = 0; j < count; j++) total += average_total[j];
+	return total / count;
+}
+void Storage::dump()
+{
+	for (int i = 0; i < count; i++)
+	{
+		cout << i + 1 << ". " << average_total[i] << endl;
+	}
+}
+
+int main()
+{
+	Storage a;
+	a.put(36.7);
+	a.put(36.9);
+	a.put(36.4);
+	a.dump();
+	cout << "평균 체온은 " << a.getAvg() << "입니다." << endl;
+}
