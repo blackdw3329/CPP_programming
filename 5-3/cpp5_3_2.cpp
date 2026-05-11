c762b15175fa139631e9eec90664e0c7bd46be6e
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-05-11 10:17:14 +0900

    Add files via upload

---
+++ 5-3/cpp5_3_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+void Uppercase(string& str);
+
+
+int main() {
+	string s1("hello");
+	cout << "변환 전 문자열" << s1 << endl;
+	Uppercase(s1);
+	cout << "변환 후 문자열" << s1 << endl;
+	return 0;
+}
+
+void Uppercase(string& str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		str[i] = toupper(str[i]);
+	}
+}
---
+++ 5-3/cpp5_3_3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void SwapString(string& str1, string& str2);
+
+
+int main() {
+	string s1(" hello ");
+	string s2(" world ");
+	cout << "교환전 문자열" << s1 << s2 << endl;
+	SwapString(s1, s2);
+	cout << "교환후 문자열" << s1 << s2 << endl;
+	return 0;
+}
+
+void SwapString(string& str1, string& str2) {
+	string tmp;
+	tmp = str1;
+	str1 = str2;
+	str2 = tmp;
+}
