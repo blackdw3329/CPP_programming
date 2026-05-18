4a734bb3e2a2f80da41a0ad31890ec62f997dd7a
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-05-18 10:03:09 +0900

    Add files via upload

---
+++ 6-1,2/cpp6_1_4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Point3D {
+	int p1, p2, p3;
+public:
+	Point3D():Point3D(0,0,0){}
+	Point3D(int p1) { this->p1 = p1; this->p2 = 0; this->p3 = 0; }
+	Point3D(int p1, int p2){ this->p1 = p1; this->p2 = p2; this->p3 = 0; }
+	Point3D(int p1, int p2, int p3){ this->p1 = p1; this->p2 = p2; this->p3 = p3; }
+	void show() {
+		cout << "3차원 점의 좌표는 (" << p1 << "," << p2 << "," << p3 << ")" << endl;
+	}
+};
+
+int main() {
+	Point3D p0;
+	p0.show();
+	Point3D p1(1);
+	p1.show();
+	Point3D p2(1, 2);
+	p2.show();
+	Point3D p3(1, 2, 3);
+	p3.show();
+	return 0;
+}
